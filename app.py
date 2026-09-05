f0f70bc8a9f2627e46b64943b19c4d0dcb193c07
hacak-ops <haneenajas0@gmail.com>
2026-09-05 21:23:52 +0530

    updated welcome updated appfile

--- app.py
+++ app.py
@@ -13,8 +13,15 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
-@app.route("/", methods=["GET", "POST"])
-def home():
+# WELCOME PAGE
+@app.route("/")
+def welcome():
+    return render_template("welcome.html")
+
+
+# OBJECT ANALYSER PAGE
+@app.route("/analyser", methods=["GET", "POST"])
+def analyser():
 
     result = ""
 
@@ -24,24 +31,19 @@ def home():
 
         if image:
 
-            filename = image.filename
-
             filepath = os.path.join(
                 app.config["UPLOAD_FOLDER"],
-                filename
+                image.filename
             )
 
             image.save(filepath)
 
-            # Convert the uploaded image
-            # into a format the AI can read
             with open(filepath, "rb") as image_file:
 
                 encoded_image = base64.b64encode(
                     image_file.read()
                 ).decode("utf-8")
 
-            # Ask the AI
             response = client.responses.create(
 
                 model="gpt-5.6-luna",
@@ -58,13 +60,11 @@ def home():
                                 "text": """
 Look at this image and identify the main object.
 
-Then give that object funny and completely
-unnecessary life advice.
+Then give that object funny and completely unnecessary life advice.
 
 Treat the object as if it has feelings.
 
-The advice should relate to the purpose
-of the object.
+Make the advice connected to what the object does.
 
 Keep the answer short.
 
