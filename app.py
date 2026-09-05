2f15f7ae42540e1fb9c4e520f9fa363115dd4472
hacak-ops <haneenajas0@gmail.com>
2026-09-06 01:14:24 +0530

    Load local environment variables

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 from openai import AuthenticationError, OpenAI
+from dotenv import load_dotenv
 import os
 import base64
+
+load_dotenv()
 
 app = Flask(__name__)
 
